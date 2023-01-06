2ab38e9527bc3b2bdbfd5550b9199de80d4cc361
SuryaRaj068 <121540596+SuryaRaj068@users.noreply.github.com>
2023-01-06 08:37:01 +0530

    Add files via upload

---
+++ Axis DAY to DAY list/ClassDemo.java
@@ -0,0 +1,28 @@
+public class ClassDemo {
+    // fields
+    // methods
+    String name = "surya";
+    int age = 34;
+    static String place;
+    // instance variable global varaible
+    // local variable
+    // static variable
+    // value will be same
+    // class name
+    // staic variable will not present inside object
+    // static members it belongs to the class
+    //
+
+    public static void  main(String args[]) {
+        ClassDemo objectName = new ClassDemo();
+        System.out.println(palce);
+        objectName.name;
+        objectName.show();
+    }
+
+    public void show() {
+        String message = "displaying data";
+        System.out.println(name);
+    }
+}
+object = new A();
---
+++ Axis DAY to DAY list/Company.java
@@ -0,0 +1,18 @@
+public class Company {
+    static String companyName = "Axis";
+    String name;
+    int age;
+    long phno;
+
+    public void createEmploye(){
+            Company obj1=new Company("Axis","manoj",21,6363837792);
+            Company obj2=new Company("Axis","Ashok",21,6363837892);
+            Company obj3=new Company("Axis","nishanth",21,6363837892);
+            obj1.name;
+            obj1.companyName="Axis1"
+            obj2.name;
+            //obj2.companyName="Axis1"
+            obj3.name;
+            //obj3.companyName="Axis1"
+    }
+}
